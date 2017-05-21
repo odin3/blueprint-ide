c3a88e2ecf6bdfae575bf2e7f8bd8ace4c922045
odin3 <ascii@live.ru>
2017-05-21 19:56:39 +0300

    Add wellcome menu and tab container

--- src/app/components/tabset/index.ts
+++ src/app/components/tabset/index.ts
@@ -1,7 +1,9 @@
 import { TabsetComponent } from './tabset.component';
+import { TabsetWellcomeComponent } from './tabset-wellcome/tabset-wellcome.component';
 
 export * from './tabset.component';
 
 export const tabsetComponents = [
+  TabsetWellcomeComponent,
   TabsetComponent
 ];
---
+++ src/app/components/tabset/tabset-wellcome/tabset-wellcome.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tabset-wellcome',
+  templateUrl: './tabset-wellcome.component.html',
+  styleUrls: ['./tabset-wellcome.component.scss']
+})
+
+export class TabsetWellcomeComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+}
